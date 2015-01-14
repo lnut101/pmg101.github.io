a058aa63289c818bbb83a83e6c6c105933faa51c
Paul Mitchell-Gears <pmg102@gmail.com>
2015-01-14 20:49:06 +0000

    Improve stubs & spies and add test

--- spec/views/TicTacToeBoardViewSpec.js
+++ spec/views/TicTacToeBoardViewSpec.js
@@ -9,24 +9,25 @@ describe('TicTacToeBoardView', function() {
 			getLayer: function() { return { draw: function() {} }; },
 			add: function() { }
 		};
-		var board = new TicTacToeBoard();
+		this.board = new TicTacToeBoard();
 		var controller = {
 			isGameOver: function() { return false; }, 
 			tryPlayMoveAt: function(position) { self.movesPlayed.push(position); }, 
 		};
 
 		// Stubs:
-		this.callbacks = [];
-		this.groupsCreated = 0;
-
-		Kinetic.Text = function() { };
-		Kinetic.Group = function() { self.groupsCreated++; };
-		Kinetic.Group.prototype.on = function(event, callback) { self.callbacks.push(callback); };
-		Kinetic.Group.prototype.add = function(o) { };
 		Kinetic.Rect = function() { };
 		Kinetic.Line = function() { };
+		this.textShown = null;
+		Kinetic.Text = function(args) { self.textShown = args.text; };
+		Kinetic.Text.prototype.text = function(text) { self.textShown = text; };
+		this.groupsCreated = 0;
+		Kinetic.Group = function() { self.groupsCreated++; };
+		this.callbacks = [];
+		Kinetic.Group.prototype.on = function(event, callback) { self.callbacks.push(callback); };
+		Kinetic.Group.prototype.add = function(o) { };
 
-		this.view = new TicTacToeBoardView(node, controller, board); 
+		this.view = new TicTacToeBoardView(node, controller, this.board); 
 	});
 
     it('renders board of nine tiles', function() {
@@ -39,6 +40,12 @@ describe('TicTacToeBoardView', function() {
     	expect(this.callbacks.length).toBe(9);
     	this.callbacks[0]();
     	expect(this.movesPlayed).toEqual([ 0 ]);
+    });
+
+    it('when board is modified, tile shows move', function() {
+    	this.view.render();
+    	this.board.squares[4].set(Player.CROSSES);
+    	expect(this.textShown).toEqual('x');
     });
 
 });
--- spec/views/TicTacToePlayerViewSpec.js
+++ spec/views/TicTacToePlayerViewSpec.js
@@ -2,16 +2,17 @@ describe('TicTacToePlayerView', function() {
 	beforeEach(function() {
 		var self = this;
 		
-		// Mocks:
-		var node = {
-			getLayer: function() { return { draw: function() {} }; },
-			add: function() { }
-		};
 		this.currentPlayer = new Observable(Player.NOUGHTS);
 
-		// Stubs:
-		this.textShown = null;
+		// Spy:
+		this.redrawCount = 0;
+		var node = {
+			getLayer: function() { return { draw: function() { self.redrawCount++; } }; },
+			add: function() { }
+		};
 
+		// Spy:
+		this.textShown = null;
 		Kinetic.Text = function(args) { self.textShown = args.text; };
 		Kinetic.Text.prototype.text = function(text) { self.textShown = text; };
 
@@ -23,10 +24,12 @@ describe('TicTacToePlayerView', function() {
     	expect(this.textShown).toEqual('o');
     });
 
-    it('when current player switches shows crosses turn', function() {
+    it('when current player switches, shows crosses turn', function() {
     	this.view.render();
+    	expect(this.redrawCount).toBe(1);
     	this.currentPlayer.set(Player.CROSSES);
     	expect(this.textShown).toEqual('x');
+    	expect(this.redrawCount).toBe(2);
     });
 
 });
